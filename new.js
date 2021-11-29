5c55f17a7eca980a6c2f12dcd4f17db0da225135
tonygbsantiago <85644672+tonygbsantiago@users.noreply.github.com>
2021-11-29 15:50:35 -0300

    Update new.js

--- new.js
+++ new.js
@@ -1,1 +1,60 @@
-// Cole aqui
+console.log("Vamos jogar!");
+console.log("Digite uma palavra: ");
+
+let secretWord = prompt();
+
+while (wordTest == false || secretWord.length == 0) {
+	console.log("A palavra precisa conter apenas letras");
+	console.log("Digite outra palavra: ");
+
+	let secretWord = prompt();
+    var wordTest = new Boolean(/^[a-zA-Z]*$/.test(secretWord));
+}
+
+secretWord = secretWord.toUpperCase();
+
+var secWordArr = secretWord.split("")
+var lives = 5;
+var counter = -1;
+var wordLenght = secWordArr.length;
+var secretArr = new Array(wordLenght).fill("_")
+console.log(secretArr)
+var guessedLatters  = [];
+var numberStore = 0;
+var victory = new Boolean(false);
+
+while(lives > 0){
+
+  console.log("atual: " + secretArr)
+  console.log("advinhe uma letra: ");
+
+  let playerGuess = prompt();
+
+  var guessTest = new Boolean(/^[a-zA-Z]*$/.test(playerGuess));
+
+  while (guessTest == false || playerGuess.length != 1) {
+      console.log("Insira apenas uma letra!")
+      playerGuess = prompt();
+      guessTest = new Boolean(/^[a-zA-Z]*$/.test(playerGuess))
+    }
+
+    playerGuess = playerGuess.toUpperCase();
+    
+    if(secWordArr.includes(playerGuess)){
+
+      var indices = [];
+      var idx = secWordArr.indexOf(playerGuess);
+      while (idx != -1) {
+        indices.push(idx);
+        idx = secWordArr.indexOf(playerGuess, idx + 1);
+        }
+      var removed = secretArr.splice(2, 0, "drum")
+        console.log(indices);
+
+      lives = lives
+    }else{
+      lives = lives-1
+      console.log("Voce errou restam: " + lives + " vidas")
+    }
+
+}
