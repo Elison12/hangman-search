0299e9c509af08cf4f7496630af5c1fb0737a0ae
Elison monteiro <56695160+Elison12@users.noreply.github.com>
2021-12-05 09:43:55 -0300

    09:43 / domingo

--- new.js
+++ new.js
@@ -28,11 +28,16 @@ $("#enterWord #submit-word").on("click", function (e) {
         window.location.reload(true);
         alert("A palavra só pode conter letras !")
     } else {
-        playGame(Sword);
+        // playGame(Sword);
+        // validLetter(Sword);
+        // console.log(answerArray.length)
     }
     $("#enterWord #word-field").hide();
     $("#enterWord #submit-word").hide();
     $("#playerOne h2").hide();
+
+    window.addEventListener('keyup', digitadas2);
+
 })
 
 
@@ -43,7 +48,7 @@ $(".alpha").on("click", function (e) {
 $(".alpha").children().on("click", letterTry);
 
 
-window.addEventListener('keyup', validLetter);
+// window.addEventListener('keyup', digitadas);
 
 
 function valid(palavra) {
@@ -63,26 +68,91 @@ function valid(palavra) {
     }
 }
 
-// function letterTry() {
-//     var guess = $(this).attr('id');
-//     console.log(guess);
-//     return guess;
+function letterTry() {
+    var guess = $(this).attr('id');
+    console.log(guess);
+    return guess;
+}
+
+
+// function validLetter(ev) {
+//     const letra = ev.key;
+//     if (ev.keyCode >= 65 && ev.keyCode <= 90) {
+//         // console.log(ev.keyCode);
+//         var lives = 5;
+//         var wordLenght = secWordArr.length;
+//         var secretArr = new Array(wordLenght).fill("_")
+//         var armLet = new Array();
+
+//         if (secretWord.includes(letra)) {
+//             console.log(true)
+//         }        
+//         // return letra;
+//     }
+// console.log(secretWord);
 // }
 
+// function digitadas(ev) {
+//     const letra = ev.key;
+//     if (answerArray.length == 0) {
+//         console.log("ainda não")
+//         return letra;
+//     } else {
+//         console.log("agora sim")
+//         validLetter(answerArray)
+//     }
+//     return letra;
+// }
 
-function validLetter(ev) {
-    const letra = ev.key;
+function digitadas2(ev) {
+    const l = ev.key;
+    console.log(l)
+    // console.log(getWord());
+    // console.log(secretWord);
+    validLetter(l, ev)
+    // return l;
+}
+
+lives = 5;
+indices = []
+
+
+function validLetter(letter, ev) {
+    if (answerArray.join('') == secretWord) {
+        console.log("parabéns, vc ganhou")
+    }
     if (ev.keyCode >= 65 && ev.keyCode <= 90) {
         // console.log(ev.keyCode);
-        var lives = 5;
-        var wordLenght = secWordArr.length;
-        var secretArr = new Array(wordLenght).fill("_")
-        var armLet = new Array();
+        // var wordLenght = secWordArr.length;
+        // var secretArr = new Array(wordLenght).fill("_")
+        // var armLet = new Array();
 
-        if (secretWord.includes(letra)) {
-            
-        }        
-        // return letra;
+        if (secretWord.includes(letter)) {
+            // console.log(true)
+            // var indices = []
+            var idx = secretWord.indexOf(letter);
+            while (idx != -1) {
+                indices.push(idx);
+                idx = answerArray.indexOf(letter, idx + 1);
+            }
+            for (var i = 0; i < indices.length; i++) {
+                var posicao = indices[i];
+                answerArray.splice(posicao, 1, letter);
+            }
+            // $(".dashes div").append(answerArray[i]);
+            lives = lives
+        }
+        else {
+            lives = lives - 1;
+            console.log("vc errou, restam " + lives + " vidas");
+        }
     }
-    // console.log(secretWord);
+    // if (secretWord.includes(letter)) {
+    //     console.log(true)
+    // }
+    if (lives == 0) {
+        console.log("vc perdeu")
+    }
+    console.log(answerArray);
 }
+    // console.log(secretWord);
