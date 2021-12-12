09eb2953db86db0563bd2e7310b1bc221f10b701
tonygbsantiago <85644672+tonygbsantiago@users.noreply.github.com>
2021-12-12 14:32:03 -0300

    Add files via upload

--- new.js
+++ new.js
@@ -1,10 +1,10 @@
 var word = $("#enterWord #word-field");
 var answerArray = [];
 secretWord = "";
-// var guessArray = [];
 var wordLength = 0;
 var letters = ""
 
+//Classe Node
 class Node {
     constructor(value) {
         this.value = value
@@ -14,12 +14,13 @@ class Node {
     }
 
 }
+//Classe BST
 class BinarySearchTree {
 
     constructor() {
         this.root = null
     }
-
+    //Procura o valor na arvore
     find(value) {
         if (!this.root) return false
 
@@ -39,7 +40,7 @@ class BinarySearchTree {
         if (!found) return undefined;
         return found
     }
-
+    //Insere o valor na arvore
     insert(value) {
         var newNode = new Node(value);
         if (this.root === null) {
@@ -65,11 +66,10 @@ class BinarySearchTree {
         }
     }
 }
-
-// var BST = new BinarySearchTree()
+//Inicia a arvore
 var arvore = new BinarySearchTree()
 
-
+//Pega a palavra e retorna ela em forma de string com secretWord e em forma de array com letters
 function getWord() {
     secretWord = word.val();
     letters = secretWord.split('');
@@ -81,13 +81,14 @@ function getWord() {
     document.getElementById("p1").innerHTML = answerArray
     return secretWord, letters;
 }
-
+//Pega a palavra apos o click no botao ok e envia para a validacao
 $("#enterWord #submit-word").on("click", function (e) {
     e.preventDefault()
     Sword = getWord();
     Sword = secretWord;
+    //Faz o teste com a palavra secreta
     var teste = valid(Sword);
-
+    //Caso falhe no teste recarrega a pagina
     if (teste == false) {
         window.location.reload(true);
         alert("A palavra só pode conter letras !")
@@ -105,9 +106,7 @@ $(".alpha").on("click", function (e) {
     e.preventDefault();
 });
 
-$(".alpha").children().on("click", letterTry);
-
-
+//Faz a validacao da palavra para ver se esta propria para o jogo
 function valid(palavra) {
     if (palavra.length == 0) {
         alert('A palavra não pode ser nula')
@@ -122,37 +121,36 @@ function valid(palavra) {
     }
 }
 
-function letterTry() {
-    var guess = $(this).attr('id');
-    console.log(guess);
-    return guess;
-}
-
-
+//Funcao responsavel por pegar as letras por meio dos clicks do teclado
 function digitadas2(ev) {
     const l = ev.key;
-    console.log(l)
 
     validLetter(l, ev)
 }
 
 lives = 5;
 indices = []
-var root = root
 
-
+//Funcao responsavel pelo funcionamento do game
 function validLetter(letter, ev) {
 
+    //Verifica se o codigo da letra escolhida pelo jogador ja foi utilizada e colocada na arvore
     if (arvore.find(ev.keyCode) == true) {
 
         fireLetterAlert(letter)
 
-    } else {
+    }
+    //Verifica se o usuario digitou uma letra que faz parte do alfabeto de a-z ou A-Z
+    else if(valid(ev.key) == false) {
+        onlyletterAlert()
+    }
+    //Vai pro funcionamento do game
+    else {
 
         if (ev.keyCode >= 65 && ev.keyCode <= 90) {
-
+            //Ve se a letra digitada faz parte da palavra secreta
             if (letters.includes(letter)) {
-
+                //Troca os underline pela letra digitada pelo usuario ja que faz parte da palavra secreta
                 var indices = []
                 var idx = letters.indexOf(letter);
                 while (idx != -1) {
@@ -165,27 +163,24 @@ function validLetter(letter, ev) {
                     document.getElementById("p1").innerHTML = answerArray
                 }
                 lives = lives
+                //Verifica se o array de respostas esta igual ao array que armazenou a palavra secreta (ambos convertidos para string)
                 if (answerArray.toString() == letters.toString()) {
-                    console.log("parabéns, vc ganhou")
                     fireSweetAlert();
 
                     setTimeout(() => {
                         window.location.reload(true);
                     }, 5000);
                 }
+                //Caso a letra digitada nao faca parte da palavra secreta perde 1 vida e faz update da imagem do boneco
             } else {
                 lives = lives - 1;
-                console.log("vc errou, restam " + lives + " vidas");
-                console.log(lives);
                 updateImage(lives);
             }
         }
-        if (lives == 0) {
-            console.log("vc perdeu")
-        }
-        console.log(answerArray);
+        //Insere o codigo da letra digitada pelo jogador na arvore
+        arvore.insert(ev.keyCode)
     }
-
+    //Funcao responsavel por dar update nas imagens
     function updateImage(lives) {
         if (lives == 4) {
             $("img").attr("src", "assets/img/life4.png");
@@ -205,28 +200,31 @@ function validLetter(letter, ev) {
             }, 5000);
         }
     }
-    arvore.insert(ev.keyCode)
 }
-
+//Mensagem quando o jogador ganha
 function fireSweetAlert() {
     Swal.fire(
         'Ufaa',
         'Você se salvou com a palavra: ' + secretWord
     )
 }
-
+//Mensagem quando o jogador perde
 function fireErrorAlert() {
     Swal.fire(
         'Faleceu',
         'A palavra era: ' + secretWord
     )
 }
-
+//Mensagem quando o jogador ja usou essa letra
 function fireLetterAlert(letter) {
     Swal.fire(
         'Ixee',
         'Você ja usou a letra: ' + letter
     )
 }
-
-
+//Mensagem quando o jogador usa alguma letra que nao faz parte do alfabeto
+function onlyletterAlert() {
+    Swal.fire(
+        'Só vale letras do alfabeto !!'
+    )
+}
